56596d9de19d6cf24e2105b937c3001ac0fc9edb
Arttu Mikkola <arttu.mikkola@windowslive.com>
2023-03-17 14:26:43 +0200

    Add font sizes

--- säätutka/tailwind.config.cjs
+++ säätutka/tailwind.config.cjs
@@ -10,9 +10,13 @@ module.exports = {
         gray: "#f8f9fa",
         white: "ffffff",
         border: "#e6e6e6",
+        text: "#70757A",
       },
       fontSize: {
         "3xl": "23pt",
+        "2xl": "19pt",
+        "4xl": "26pt",
+        xl: "15pt",
       },
     },
   },
