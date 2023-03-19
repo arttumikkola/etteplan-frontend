c67699440813af3414c8dd634152c0d150fabed3
Arttu Mikkola <arttu.mikkola@windowslive.com>
2023-03-19 18:33:39 +0200

    Add colors

--- säätutka/tailwind.config.cjs
+++ säätutka/tailwind.config.cjs
@@ -11,12 +11,18 @@ module.exports = {
         white: "ffffff",
         border: "#e6e6e6",
         text: "#70757A",
+        text2: "#262626",
+        blue: "#E5F6FD",
       },
       fontSize: {
         "3xl": "23pt",
         "2xl": "19pt",
         "4xl": "26pt",
         xl: "15pt",
+        sm: "10pt",
+      },
+      width: {
+        20: "69px",
       },
     },
   },
