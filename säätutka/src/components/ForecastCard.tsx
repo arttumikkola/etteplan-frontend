3cf1a2fe1bb0a80c9a2faf1bf3286594abc4acb4
Arttu Mikkola <arttu.mikkola@windowslive.com>
2023-03-20 14:00:18 +0200

    Update to display real data

--- säätutka/src/components/ForecastCard.tsx
+++ säätutka/src/components/ForecastCard.tsx
@@ -1,16 +1,59 @@
-export const ForecastCard = () => {
+type ForeCast = {
+  list: {
+    dt: number;
+    main: {
+      temp: number;
+      humidity: number;
+    };
+    weather: {
+      icon: string;
+    }[];
+    wind: {
+      speed: number;
+    };
+    rain: {
+      "3h": number;
+    };
+  }[];
+};
+
+function formatTime(timestamp: number) {
+  const date = new Date(timestamp * 1000);
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  return `${hours}:${minutes}`;
+}
+
+export const ForecastCard = (forecast: ForeCast) => {
+  const forecasts = forecast.list.slice(0, 5);
   return (
-    <div className="h-34 w-20 border border-border rounded-md flex flex-col items-center justify-center bg-white">
-      <div>
-        <p className="m-1 text-base text-text">15:00</p>
-        <img src="https://openweathermap.org/img/wn/01d.png" alt="Clouds" />
-        <p className="text-xl text-text2 m-1">-1°C</p>
-      </div>
-      <div className="bg-blue min-w-full flex flex-col items-center justiffy-center">
-        <p className="m-px text-sm text-text">2.1 m/s</p>
-        <p className="m-px text-sm text-text">5%</p>
-        <p className="m-px text-sm text-text">1 mm</p>
-      </div>
+    <div className="flex flex-row items-center justify-center space-x-1">
+      {forecasts.map((item, index) => (
+        <div
+          key={index}
+          className="h-34 w-20 border border-border rounded-md flex flex-col items-center justify-center bg-white"
+        >
+          <div>
+            <p className="m-1 text-base text-text">{formatTime(item.dt)}</p>
+            <img
+              src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+              alt="Weather"
+            />
+            <p className="text-xl text-text2 m-1">
+              {item.main.temp.toFixed(0)} °C
+            </p>
+          </div>
+          <div className="bg-blue min-w-full flex flex-col items-center justify-center">
+            <p className="m-px text-sm text-text">
+              {item.wind.speed.toFixed(1)} m/s
+            </p>
+            <p className="m-px text-sm text-text">{item.main.humidity} %</p>
+            <p className="m-px text-sm text-text">
+              {item.rain?.["3h"] || 0} mm
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
